70ad36ec4b926639dd170d0590bd0979bf218411
Sungjoon Kim <ted@bitflow.ai>
2021-02-18 17:26:25 +0900

    initial commit

---
+++ frontend/vue.config.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+module.exports = {
+    outputDir: path.resolve(__dirname, "../src/main/resources/META-INF/resources"),
+    devServer: {
+        // port: process.env.VUE_APP_PORT || 3000
+    }
+}
---
+++ src/main/java/org/acme/hibernate/reactive/Fruit.java
@@ -0,0 +1,37 @@
+package org.acme.hibernate.reactive;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@ToString
+@Table(name = "known_fruits")
+@NamedQuery(name = Fruit.FIND_ALL, query = "SELECT f FROM Fruit f ORDER BY f.name")
+public class Fruit {
+    public static final String FIND_ALL = "Fruits.findAll";
+
+    @Id
+    @SequenceGenerator(name = "fruitsSequence", sequenceName = "known_fruits_id_seq", allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "fruitsSequence")
+    private Integer id;
+
+    @Column(length = 40, unique = true)
+    private String name;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+}
